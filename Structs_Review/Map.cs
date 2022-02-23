db5aca747d0c17223bcc0374a88f8da007a62587
Kyle Dixon <90637263+KyleDixonVO@users.noreply.github.com>
2022-02-23 09:58:33 -0400

    Finished review lesson

---
+++ Structs_Review/Map.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs_Review
+{
+    class Map
+    {
+        struct Tile
+        {
+            public char character;
+            public ConsoleColor Fore;
+            public ConsoleColor Back;
+        }
+
+        Tile[,] map;
+
+        public Map()
+        {
+            map = new Tile[100, 100];
+        }
+
+        public void Draw()
+        {
+            int x = 0;
+            int y = 0;
+
+            Console.ForegroundColor = map[x, y].Fore;
+            Console.ForegroundColor = map[x, y].Back;
+
+        }
+    }
+}
--- Structs_Review/Program.cs
+++ Structs_Review/Program.cs
@@ -10,6 +10,24 @@ namespace Structs_Review
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Howdy!");
+
+            Vector2 position;
+
+            position.x = 0;
+            position.y = 0;
+
+            MoveTo(position);
+
+            Console.WriteLine();
+            Console.WriteLine("Press any input to exit");
+            Console.ReadKey(true);
+        }
+
+        private static void MoveTo(Vector2 position)
+        {
+            Console.WriteLine("About to move to (" + position.x + "," + position.y + ")");
+            Console.WriteLine("About to move to (" + position + ")");
         }
     }
 }
